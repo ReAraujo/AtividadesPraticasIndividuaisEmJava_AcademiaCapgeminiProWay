2d6eb58b0fe06ff89f42a63cd06652df2f4ca520
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-14 16:21:55 -0300

    alteração nome classe

--- AtividadePratica01/AtividadePratica01.java
+++ AtividadePratica01/AtividadePratica01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AtividadPratica01 {
+public class AtividadePratica01 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
