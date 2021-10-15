0ec89e7e2dd6e7da23e3965dc7f886e0cda3b587
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-15 07:35:13 -0300

    modificações para solução de bugs

--- AtividadePratica23/model/Livro.java
+++ AtividadePratica23/model/Livro.java
@@ -38,6 +38,10 @@ public class Livro extends Base {
         return categoria;
     }
 
+    public String toString() {
+        return "ID: "+this.getId()+"Nome: "+this.getNome()+"\nAutor: "+this.getAutor();
+    }
+
 
 
 
