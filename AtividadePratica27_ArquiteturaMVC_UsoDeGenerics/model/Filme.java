06a6ae9b2a5c7e0abbc739bca0dd59def4d05f6f
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-20 16:26:49 -0300

    criação dos métodos equals e toString

--- AtividadePratica27_ArquiteturaMVC_UsoDeGenerics/model/Filme.java
+++ AtividadePratica27_ArquiteturaMVC_UsoDeGenerics/model/Filme.java
@@ -5,5 +5,29 @@ public class Filme {
     public String titulo;
     public String diretor;
     public String dataLancamento;
+    public String genero;
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Filme){
+            Filme outro = (Filme)obj;
+            if(this.id == outro.id){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String impressao1 = "\n\tTítulo: " + this.titulo +
+                            "\n\tDiretor(es): " + this.diretor +
+                            "\n\tData de lançamento: " + this.dataLancamento +
+                            "\n\tGênero: " + this.genero;
+        return impressao1;        
+    }
+
+
+
     
 }
--- AtividadePratica27_ArquiteturaMVC_UsoDeGenerics/model/Serie.java
+++ AtividadePratica27_ArquiteturaMVC_UsoDeGenerics/model/Serie.java
@@ -5,6 +5,14 @@ public class Serie extends Filme{
     public int quantidadeDeTemporadas;
     public int quantidadeDeEpisodios;
 
+    @Override
+    public String toString() {
+        String impressao2 = "\n\tEmissora: " + this.emissora +
+                            "\n\tQuantidade de Temporadas: " + this.quantidadeDeTemporadas +
+                            "\n\tQuantidade de Episódios: " + this.quantidadeDeEpisodios;
+        return super.toString() + impressao2;
+    }
+
     
     
 }
