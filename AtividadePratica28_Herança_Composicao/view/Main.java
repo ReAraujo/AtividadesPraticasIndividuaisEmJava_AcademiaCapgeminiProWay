eb49a858275f0066ad619e9ce2b48d9503277447
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-21 16:15:24 -0300

    criação das classes controllers e main

---
+++ AtividadePratica28_Herança_Composicao/view/Main.java
@@ -0,0 +1,13 @@
+/* _  Crie um sistema de cadastro de Pessoa Física e Pessoa Jurídica.
+  _  Tanto Pessoa Física como Pessoa Jurídica devem possuir Endereço.
+  _  O sistema deve utilizar o padrão de arquitetura MVC
+  _  Os modelos devem possuir um id e outros atributos públicos de sua escolha.
+  _  As controllers devem possuir os quatro métodos de CRUD e deve utilizar um ArrayList privado para manter os dados. (Utilize o conceito de Generics nas Controllers)
+  _  A view deve testar as operações de CRUD para cada controller (Utilize dados fixo, sem entrada do usuário).
+*/
+
+package view;
+
+public class Main {
+    
+}
