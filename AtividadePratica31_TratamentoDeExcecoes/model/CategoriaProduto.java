458a94646b20ac1703c4a71f326dabec84e24c2a
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-26 10:46:37 -0300

    croação das classes na model

---
+++ AtividadePratica31_TratamentoDeExcecoes/model/CategoriaProduto.java
@@ -0,0 +1,6 @@
+package model;
+
+public class CategoriaProduto extends Base { // Herança
+
+    
+}
---
+++ AtividadePratica31_TratamentoDeExcecoes/model/Produto.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Produto extends Base { // Herança
+    public CategoriaProduto categoriaProduto; // Composição 
+    public String nomeProduto;
+    public String descricaoProduto;
+    public double valorProduto;
+}
