b1efbcdff1ced352ae0efd347f6f9ac536fa7828
Renata Araujo <renata.araujofreitas@gmail.com>
2021-10-26 10:20:26 -0300

    criação da classe categoria na model e CategoriaController

--- AtividadePratica31_TratamentoDeExcecoes/model/Produto.java
+++ AtividadePratica31_TratamentoDeExcecoes/model/Produto.java
@@ -1,7 +1,14 @@
 package model;
 
 public class Produto{
-
+    public Categoria categoriaProduto;
+    public String nome;
+    public String descricao;
+    public double valor;
 
     
+     
+
+
+
 }
