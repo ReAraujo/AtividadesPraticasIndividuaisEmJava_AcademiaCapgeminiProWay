c335220058c3b54e6ac420f1047b37585b1367e8
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-03 14:57:19 -0300

    atividade 38 - concluída

---
+++ AtividadePratica38_Java.IO/view/Leitura.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Leitura {
+    public static void main(String[] args) {
+
+        // Fluxo de entrada através de um arquivo:    
+       try {
+            FileInputStream fis = new FileInputStream("dados/leitura.txt"); // entrada de bytes
+            InputStreamReader isr = new InputStreamReader(fis); // leitor de Stream - converte byte para character 
+            //char[] dados =  new char[90];
+            //isr.read(dados, 0, 89);
+            //System.out.println(dados); // leitura por character 
+            BufferedReader bRead = new BufferedReader(isr); // faz o acúmulo de character para a leitura de linha ou de totalidade dele
+
+            while(bRead.ready()){
+                String texto = bRead.readLine();
+                System.out.println(texto);
+            }
+            bRead.close();
+
+            System.out.println("\nO arquivo foi aberto com sucesso!");
+        } catch (FileNotFoundException ex) {
+            System.out.println("\nO arquivo não foi encontrado.Que tal procurar novamente?");
+        } catch (IOException ex) {
+            System.out.println("\nDesculpe, não foi possível ler o arquivo.");
+        }
+         
+
+
+
+
+
+        
+    }
+    
+}
