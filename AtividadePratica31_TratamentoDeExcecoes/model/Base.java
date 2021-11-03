acab1f8e70b305f7240bbd9fab3376d3b5d9341d
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-03 10:02:00 -0300

    commitando

--- AtividadePratica31_TratamentoDeExcecoes/model/Base.java
+++ AtividadePratica31_TratamentoDeExcecoes/model/Base.java
@@ -2,5 +2,17 @@ package model;
 
 public class Base {
     public String id;
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Base){
+            Base outro = (Base)obj;
+            if(this.id == outro.id){
+                return true;
+            }
+        }        
+        return false;
+    }
     
 }
