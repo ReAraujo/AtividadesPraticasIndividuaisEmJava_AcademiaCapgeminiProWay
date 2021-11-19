b370ed2addcccaeb30fa9cd0ed6145daf0c7cedd
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-19 13:31:04 -0300

    Cópia das classes da ATP47

---
+++ AtividadePratica48/View_Classe1_Select.java
@@ -0,0 +1,51 @@
+/* _  Crie uma cópia das classes criadas durante a atividade ATP47.
+   _  Altere a classe1 para utilizar preparedStatement.
+   _  Altere a classe2 para receber os parâmetros nome e descrição através de variáveis e troque o Statement para PreparedStatement.
+   _  Altere a classe3 para  receber os nomes das categorias que serão alteradas, através de variáveis e troque o Statement para PreparedStatement.
+   _  Altere a classe4 para receber o Id da categorias a ser deletada através de variável e troque o Statement para PreparedStatement..
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class View_Classe1_Select {
+    public static void main(String[] args) {
+        try {
+            String driverType = "jdbc";
+            String driverName = "postgresql";
+            String host = "localhost";
+            int port = 5432;
+            String database = "postgres";
+            String user = "postgres";
+            String pwd = "123789";
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+            String sql = "SELECT id, nome FROM Categoria";
+
+            // Conectando no Banco de Dados
+            Connection conn = DriverManager.getConnection(connectionString, user, pwd);
+
+            // Statement = Executes the given SQL statement (comando SQL)
+            Statement statement = conn.createStatement();
+            statement.execute(sql);
+
+            // Result = Retorno da consulta
+            ResultSet result = statement.getResultSet();
+
+            // Imprimindo o resultado de Result (retorno)
+            while (result.next()) { 
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                System.out.printf("|ID: %d - |Nome: %s \n", id, nome);            
+            }
+
+            // Fechando a conexão 
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println("Desculpe, não foi possível conectar.");
+        }
+    }
+}
---
+++ AtividadePratica48/View_Classe4_Delete.java
@@ -0,0 +1,46 @@
+/* _  Crie uma cópia das classes criadas durante a atividade ATP47.
+   _  Altere a classe1 para utilizar preparedStatement.
+   _  Altere a classe2 para receber os parâmetros nome e descrição através de variáveis e troque o Statement para PreparedStatement.
+   _  Altere a classe3 para  receber os nomes das categorias que serão alteradas, através de variáveis e troque o Statement para PreparedStatement.
+   _  Altere a classe4 para receber o Id da categorias a ser deletada através de variável e troque o Statement para PreparedStatement..
+*/
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class View_Classe4_Delete {
+   public static void main(String[] args) {
+      try {
+         String driverType = "jdbc";
+         String driverName = "postgresql";
+         String host = "localhost";
+         int port = 5432;
+         String database = "postgres";
+         String user = "postgres";
+         String pwd = "123789";
+         String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+         String sql1 = "DELETE FROM Categoria WHERE nome = 'Cabos e utilitários'";
+         String sql2 = "DELETE FROM Categoria WHERE nome = 'Móveis de Escritório'";
+
+         // Conectando no Banco de Dados
+         Connection conn = DriverManager.getConnection(connectionString, user, pwd);
+
+         // Statement = Executes the given SQL statement (comando SQL)
+         Statement statement = conn.createStatement();
+         statement.execute(sql1);
+         statement.execute(sql2);
+
+         // Verificando quantas linhas foram afetadas pelo método DELETE
+         int linhasAfetadas = statement.getUpdateCount();
+         System.out.println(linhasAfetadas);
+
+         // Fechando a conexão 
+         conn.close();
+
+     } catch (SQLException e) {
+         System.out.println("Desculpe, não foi possível conectar.");
+     }
+   }
+}   
