ac016bf2962d98a48064934b45cad0ab0a8a4afe
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-19 06:59:58 -0300

    Finalização da classe Select e testes de impressão em console

--- AtividadePratica46_JDBC/View_Classe1_Select.java
+++ AtividadePratica46_JDBC/View_Classe1_Select.java
@@ -5,24 +5,50 @@
    _  Na classe1, liste todos os dados de todas as colunas da tabela produto.
    _  Na classe2, insira um novo produto na tabela e imprima o id gerado.
    _  Na classe3, altere todos os produtos para a mesma categoria e exiba a quantidade de linhas afetadas.
+   _  Na classe4, delete um produto pelo id.
 */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class View_Classe1_Select {
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123789");
+            String driverType = "jdbc";
+            String driverName = "postgresql";
+            String host = "localhost";
+            int port = 5432;
+            String database = "postgres";
+            String user = "postgres";
+            String pwd = "123789";
+            String connectionString = String.format("%s:%s://%s:%d/%s", driverType, driverName, host, port, database);
+
+            // Conectando no Banco de Dados
+            Connection conn = DriverManager.getConnection(connectionString, user, pwd);
+
+            // Statement = Executes the given SQL statement (comando SQL)
+            Statement statement = conn.createStatement();
+            statement.execute("SELECT * from Categoria");
+
+            // Result = Retorno da consulta
+            ResultSet result = statement.getResultSet();
+
+            // Imprimindo o resultado de Result (retorno)
+            while (result.next()) { 
+                int id = result.getInt("id");
+                String nome = result.getString("nome");
+                String descricao = result.getString("descricao");
+                System.out.printf("|ID: %d - |Nome: %s - |Descrição: %s \n", id, nome, descricao);            
+            }
+
+            // Fechando a conexão 
+            conn.close();
+
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            System.out.println("Desculpe, não foi possível conectar.");
         }
-            
-
-
-        
-
     }
 }
