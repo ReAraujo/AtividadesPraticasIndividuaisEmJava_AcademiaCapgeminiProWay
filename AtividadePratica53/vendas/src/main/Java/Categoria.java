95be9a5c30fc26548f4b6aa416f680aff44db19e
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-22 14:34:11 -0300

    ATP53 - Conclusão e testes realizados

--- AtividadePratica53/vendas/src/main/Java/Categoria.java
+++ AtividadePratica53/vendas/src/main/Java/Categoria.java
@@ -1,8 +1,8 @@
 /* _  Crie um projeto maven utilizando o archetype: maven-archetype-webapp.
-_  Utilize o projeto construido ate a atividade ATP52 como base para este projeto.
-_  Adicione ao POM.XML as dependencias e plugins necessários para criar e rodar uma aplicação java web com servlet no tomcat.
-_  Crie uma página principal com links para /<nome_app>/categoria e /<nome_app>/produto.
-_  Crie dois servlets que respondem aos endereços /categoria e /produto. Estes servlets devem retornar uma frase simples com os nomes: Módulo Categoria e Módulo Produto.
+   _  Utilize o projeto construido ate a atividade ATP52 como base para este projeto.
+   _  Adicione ao POM.XML as dependencias e plugins necessários para criar e rodar uma aplicação java web com servlet no tomcat.
+   _  Crie uma página principal com links para /<nome_app>/categoria e /<nome_app>/produto.
+   _  Crie dois servlets que respondem aos endereços /categoria e /produto. Estes servlets devem retornar uma frase simples com os nomes: Módulo Categoria e Módulo Produto.
 */
 
 import java.io.IOException;
