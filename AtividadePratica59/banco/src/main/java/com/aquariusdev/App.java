c0eb8a02fd0605cc8ed51cf74eda610c9a73e830
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-25 15:14:47 -0300

    Criação do Maven Project

---
+++ AtividadePratica59/banco/src/main/java/com/aquariusdev/App.java
@@ -0,0 +1,13 @@
+package com.aquariusdev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
