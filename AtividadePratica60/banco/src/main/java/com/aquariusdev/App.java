120f5c5ade51c12be6e0f67dd723412bc7cab8f1
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-26 10:01:14 -0300

    Métodos read, update e delete do CRUD testados com suceso

--- AtividadePratica60/banco/src/main/java/com/aquariusdev/App.java
+++ AtividadePratica60/banco/src/main/java/com/aquariusdev/App.java
@@ -7,40 +7,23 @@
 
 package com.aquariusdev;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import com.aquariusdev.dao.CategoriaDao;
 import com.aquariusdev.models.Categoria;
 
 public class App {
     public static void main( String[] args ){
         // Criando objeto da Classe Categoria via models:
         Categoria model = new Categoria(); 
-        model.setNome("Teste 3 JPA - Nome");
-        model.setDescricao("Teste 3 JPA - Descricao");
+        model.setId(39);
+        model.setNome("Teste 6 JPA - Nome");
+        model.setDescricao("Teste 6 JPA - Descricao");
 
-        // Criando a Fábrica de Conexões via chave "banco":
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("banco"); 
-        // Criando a conexão com o DB:
-        EntityManager entMan = factory.createEntityManager(); 
-        
-        List<Categoria> lista = entMan.createQuery("SELECT c FROM Categoria c ORDER BY id", Categoria.class).getResultList(); 
-        // .createQuery() = Criando uma listagem - deve ser adicionado o nome da Classe como se fosse o nome da tabela (classe: Categoria.java):
-        // .getResultList() = receber os resultados transformados já em formato lista
+        // Criando objeto da Classe CategoriaDao:
+        CategoriaDao dao = new CategoriaDao();
 
-        // como o controle será local, conforme adicionado no arquivo persistence.xml (transaction-type="RESOURCE_LOCAL") 
-        // será necessário entãoé necessário abrir uma conexão (.getTransaction().begin()) e fazer o commit dela (.getTransaction().commit()):
-        // fazendo o controle local da conexão = abertura/start de conexão:
-        entMan.getTransaction().begin();  
-        // .persist() = Persistindo a entidade 'model' - seria somente preciso esse passo se o controle estivesse pelo Tomcat:
-        entMan.persist(model); 
-        // fazendo o controle local da conexão = fechamento da conexão:
-        entMan.persist(model); 
-        
-        for (Categoria categoria : lista) {
+        dao.delete(39); // Persistiu no DB e gerou um ID
+
+        for (Categoria categoria : dao.read()) {
             System.out.printf("ID: %d | Nome: %s | Descrição: %s \n",categoria.getId(), categoria.getNome(), categoria.getDescricao()); 
         }
 
