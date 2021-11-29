72c2d0d91490444ba075b0daf79d4e9de7754f0c
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-29 17:11:50 -0300

    ATP64 concluída e testes em web realizados com sucesso

--- AtividadePratica64/filmes/src/main/java/com/aquariusdev/filmes/model/Filme.java
+++ AtividadePratica64/filmes/src/main/java/com/aquariusdev/filmes/model/Filme.java
@@ -12,39 +12,39 @@ public class Filme {
 
     // Encapsulamento:
     @Id
-    // Informando que a coluna 'id' é auto gerada no padrão 'Identity':
-    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Informando que a coluna 'id' é auto gerada no padrão 'Identity'
     private int id;
     private String titulo;
     private String diretor;
     private String genero;
 
     // Métodos getters (métodos de acesso) e setters (métodos de modificação):
-    public int getId() {
-        return this.id;
-    }
     public void setId(int id) {
         this.id = id;
     }
-
-    public String getTitulo() {
-        return this.titulo;
+    public int getId() {
+        return this.id;
     }
+
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
-    public String getDiretor() {
-        return this.diretor;
+    public String getTitulo() {
+        return this.titulo;
     }
+
     public void setDiretor(String diretor) {
         this.diretor = diretor;
     }
-
-    public String getGenero() {
-        return this.genero;
+    public String getDiretor() {
+        return this.diretor;
     }
+
     public void setGenero(String genero) {
         this.genero = genero;
     }
+    public String getGenero() {
+        return this.genero;
+    }
+
 }
