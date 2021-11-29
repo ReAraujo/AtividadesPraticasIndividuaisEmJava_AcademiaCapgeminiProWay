d161e9a89b384670606a1a7b757bb6a88b332864
Renata Araujo <renata.araujofreitas@gmail.com>
2021-11-29 17:40:05 -0300

    Adequação do Lombok na classe Filme (model) e troca para anotação dos métodos @Getter e @Setter

--- AtividadePratica65/filmes/src/main/java/com/aquariusdev/filmes/model/Filme.java
+++ AtividadePratica65/filmes/src/main/java/com/aquariusdev/filmes/model/Filme.java
@@ -6,45 +6,25 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity // Informando que é uma entidade de tabela do DB
 @Table(name = "filme") // Informando que a tabela possui nome "filme"
 public class Filme {
-
     // Encapsulamento:
+    // Anotações @Getter @Setter = Métodos getters (métodos de acesso) e setters (métodos de modificação) inicializados pelo Lombok
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Informando que a coluna 'id' é auto gerada no padrão 'Identity'
+    @Getter @Setter
     private int id;
+
+    @Getter @Setter
     private String titulo;
+
+    @Getter @Setter
     private String diretor;
+
+    @Getter @Setter
     private String genero;
-
-    // Métodos getters (métodos de acesso) e setters (métodos de modificação):
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getId() {
-        return this.id;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getTitulo() {
-        return this.titulo;
-    }
-
-    public void setDiretor(String diretor) {
-        this.diretor = diretor;
-    }
-    public String getDiretor() {
-        return this.diretor;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-    public String getGenero() {
-        return this.genero;
-    }
-
 }
