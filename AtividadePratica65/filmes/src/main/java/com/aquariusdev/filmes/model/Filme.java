7a518aaab78b3aad16734b42d395aa5c4f8f0423
Renata Araujo <renata.araujofreitas@gmail.com>
2021-12-01 08:09:49 -0300

    ATP65 concluída e testes em web realizados com sucesso

--- AtividadePratica65/filmes/src/main/java/com/aquariusdev/filmes/model/Filme.java
+++ AtividadePratica65/filmes/src/main/java/com/aquariusdev/filmes/model/Filme.java
@@ -1,3 +1,17 @@
+/* _  Utilize como base o código gerado na ATP64.
+   _  Adicione a dependencia do lombok no pom.xml, realize o clean e install do maven.
+   _  Ajuste as classes de modelo para utilizarem as anotações de getters e setters do lombok.
+   _  Adicione o plugin para VS Code do lombok (Lombok Annotations Support for VS Code)
+   _  Teste o projeto para verificar se tudo continua funcionando corretamente com as alterações do lombok.
+   _  Crie um template para ser usado como base para outros templates.
+   _  Crie um fragment para o head do html, nele importe um css local e o css do bootstrap. O fragment deve receber como parâmetro o título da página.
+   _  Crie um fragment para o header da página e nele crie um cabeçalho do bootstrap.
+   _  Utilize os dois fragments nos demais templates.
+   _  Ajuste a tela de listagem de clientes para utilizar o estilo do bootstrap.
+   _  Ajuste a tela de cadastro e edição de clientes para utilizar o estilo do bootstrap.
+*/
+
+
 package com.aquariusdev.filmes.model;
 
 import javax.persistence.Entity;
