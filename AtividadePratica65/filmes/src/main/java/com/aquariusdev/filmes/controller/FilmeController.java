3b83c156d6f29b6b68b5946a25ff4076d8c65a57
Renata Araujo <renata.araujofreitas@gmail.com>
2021-12-06 11:00:26 -0300

    ATP65 concluída e testes em web realizados com sucesso

--- AtividadePratica65/filmes/src/main/java/com/aquariusdev/filmes/controller/FilmeController.java
+++ AtividadePratica65/filmes/src/main/java/com/aquariusdev/filmes/controller/FilmeController.java
@@ -23,7 +23,7 @@ public class FilmeController {
         this.repository = repository;
     }
 
-    @GetMapping("/filme") // Mapeando requisição tipo 'get' para a rota "/cliente" - tipo 'get' mostra os parâmetros inseridos na url
+    @GetMapping("/filme") // Mapeando requisição tipo 'get' para a rota "/filme" - tipo 'get' mostra os parâmetros inseridos na url
     public String filmes(Model req){
         List<Filme> lista = (List<Filme>)repository.findAll();
         req.addAttribute("filme", lista);
@@ -37,7 +37,7 @@ public class FilmeController {
         return "filmes-form";
     }
 
-    @PostMapping("/filme/salvar") // Mapeando requisição tipo 'post' para rota "/cliente/salvar" - tipo 'post' = mantem as informações ocultas na url
+    @PostMapping("/filme/salvar") // Mapeando requisição tipo 'post' para rota "/filme/salvar" - tipo 'post' = mantem as informações ocultas na url
     public String salvar(Filme model){
         repository.save(model);
         return "redirect:/filme";
