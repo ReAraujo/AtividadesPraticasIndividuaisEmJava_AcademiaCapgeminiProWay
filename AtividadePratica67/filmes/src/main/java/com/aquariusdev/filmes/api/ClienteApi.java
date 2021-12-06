b94b0394b3ca7f77e44651d0dcde25abfebf5c6c
Renata Araujo <renata.araujofreitas@gmail.com>
2021-12-06 13:23:31 -0300

    ATP67 concluída e testes dos métodos CRUD pela classe ClienteApi pelo Insomnia realizados com sucesso

--- AtividadePratica67/filmes/src/main/java/com/aquariusdev/filmes/api/ClienteApi.java
+++ AtividadePratica67/filmes/src/main/java/com/aquariusdev/filmes/api/ClienteApi.java
@@ -12,32 +12,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // Anotação @RestController = responde e recebe no formato JSON
+@RequestMapping("/api/cliente")
 public class ClienteApi {
     @Autowired // Anotação @Autowired = ao invés de ter o método construtor para fazer a injeção de dependências, basta ter essa anotação
     private ClienteRepository repository;
 
-    @GetMapping("/api/cliente") 
-    public List<Cliente> clientes(){
-        List<Cliente> lista = (List<Cliente>)repository.findAll();
-        return lista; 
+    @GetMapping 
+    public List<Cliente> clientes(String nome){
+        if(nome != null){
+            return (List<Cliente>)repository.findByNomeCompleto(nome);
+        }
+        return (List<Cliente>)repository.findAll(); 
     }
 
-    @PostMapping("/api/cliente")
+    @PostMapping
     public String salvar(@RequestBody Cliente model){ // Anotação @RequestBody = Cliente está vindo do corpo da requisição
         repository.save(model);
         return "O cadastro do cliente foi salvo com sucesso!";
     }
 
-    @DeleteMapping("/api/cliente/{id}")
+    @DeleteMapping("/{id}")
     public String deletar(@PathVariable int id){ 
         repository.deleteById(id);
         return "O cadastro do cliente foi deletado com sucesso!";
     }
 
-    @PutMapping("/api/cliente/{id}")
+    @PutMapping("/{id}")
     public String update(@RequestBody Cliente model, @PathVariable int id){
         // validação se o mesmo ID que foi enviado na requisição da url seja o mesmo do model:
         if(model.getId() == id){
--- AtividadePratica67/filmes/src/main/java/com/aquariusdev/filmes/repository/ClienteRepository.java
+++ AtividadePratica67/filmes/src/main/java/com/aquariusdev/filmes/repository/ClienteRepository.java
@@ -1,9 +1,33 @@
+/* _  Utilize como base o código gerado na ATP66.
+   _  Modifique o método de listagem de clientes para que retorne todos os clientes ordenados por id em ordem crescente.
+   _  Altere o método de listagem de clientes para que seja possivel filtrar os clientes pelo nome.
+*/
+
 package com.aquariusdev.filmes.repository;
+
+import java.util.List;
 
 import com.aquariusdev.filmes.model.Cliente;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ClienteRepository extends CrudRepository<Cliente, Integer>{
-    
+public interface ClienteRepository extends PagingAndSortingRepository<Cliente, Integer>{
+
+    // Classe de ponto comum de WEB e API (controller e API), por isso cria-se aqui os métodos de listagem:
+
+    // Modifique o método de listagem de clientes para que retorne todos os clientes ordenados por id em ordem crescente:
+    // ordenando por ID:
+    @Override
+    default Iterable<Cliente> findAll() {
+        return findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+    // extensão da interface PagingAndSortingRepositorypara poder paginar e ordenar os dados - ela extende de CrudRepository<T, ID>
+    // classe Sort.by = .by = pelo o que deseja ordenar 
+    // dois parâmetros: como quer ordenar (Sort.Direction.ASC)(ASC do menor para o maior) e por qual coluna quer ordenar ("id")
+
+    // Altere o método de listagem de clientes para que seja possivel filtrar os clientes pelo nome:
+    List<Cliente> findByNomeCompleto(String nome);
+    // o JPA faz automático a busca quando usa-se o find + nome da propriedade
+   
 }
